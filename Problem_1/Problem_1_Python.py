5251b2d9c521972b3ba939e37b64f8a90bde6f40
JR0901 <78112670+JR0901@users.noreply.github.com>
2022-06-27 13:59:11 +0100

    Update Problem_1_Python.py

--- Problem_1/Problem_1_Python.py
+++ Problem_1/Problem_1_Python.py
@@ -3,10 +3,3 @@ for i in range (1000):
     if (i%3 == 0) or (i%5 == 0):
         total += i
 print(total)
-
-# int total = 0; 
-#         for (int i=1; i<1000; ++i) { 
-#             if (i%3 == 0 || i%5 == 0) { 
-#                 total += i; 
-#             } 
-#         }
